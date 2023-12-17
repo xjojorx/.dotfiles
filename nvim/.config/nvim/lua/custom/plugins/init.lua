e293bc93bfd21e68ff2ec4930afd20663ae87a91
Jojor <shadowcrak@gmail.com>
2023-12-17 22:09:54 +0100

    add nvim (still only lightly modified from kickstart.nvim)

---
+++ nvim/.config/nvim/lua/custom/plugins/init.lua
@@ -0,0 +1,22 @@
+-- You can add your own plugins here or in other files in this directory!
+--  I promise not to create any merge conflicts in this directory :)
+--
+-- See the kickstart.nvim README for more information
+
+return {
+  {"neovim/nvim-lspconfig"},
+  {"simrat39/rust-tools.nvim"},
+  {"nvim-lua/plenary.nvim"},
+  {"mfussenegger/nvim-dap"},
+  {
+    "nvim-tree/nvim-tree.lua",
+    version = "*",
+    lazy = false,
+    dependencies = {
+      "nvim-tree/nvim-web-devicons",
+    },
+    config = function()
+      require("nvim-tree").setup {}
+    end,
+  },
+}
